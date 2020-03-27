f15b701a935010bdfbb56ecaf6b687682d20bed9
khalilbouch <46937290+khalilbouch@users.noreply.github.com>
2020-03-27 20:05:18 +0100

    - Afficher et supprimer Location

--- src/VelotnBundle/Controller/LocationController.php
+++ src/VelotnBundle/Controller/LocationController.php
@@ -4,6 +4,7 @@ namespace VelotnBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use VelotnBundle\Entity\Location;
@@ -33,4 +34,57 @@ public function AjouterLocationAction(Request $request)
     $em->flush();
     return $this->redirectToRoute('index');
 }
+    /**
+     * @Route("/Location",name="location")
+     */
+    public function PromotionAction()
+    {
+        $locations= $this->getDoctrine()->getRepository(Location::class)->findAll() ;
+        return $this->render('@Velotn/Front/listelocation.httml.twig', array(
+            'locations'=>$locations
+        ));
+    }
+
+    /**
+     * @param $id
+     * @Route("/SupprimerLocation/{id}",name="supprimerloco")
+     * @return RedirectResponse
+     */
+    public function SupprimerLocationAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $location = $em->getRepository(Location::class)->find($id);
+        $em->remove($location);
+        $em->flush();
+        return $this->redirectToRoute('location');
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @Route("/ModifierLocation/{id}",name="modifierloco")
+     * @return RedirectResponse|Response
+     * @throws \Exception
+     */
+    public function ModifierLocationAction(Request $request,$id)
+    {
+        $em=$this->getDoctrine()->getManager();
+
+        $loctions= $em->getRepository(Location::class)->find($id);
+
+        if($request->isMethod("POST")) {
+            $loctions->setDateDebut(new \DateTime($request->request->get('dated')));
+            $loctions->setDateFin(new \DateTime($request->request->get('datef')));
+            $em->flush();
+            return $this->redirectToRoute('location');
+        }
+        return $this->render('@Velotn/Front/ModifierLocation.html.twig', array(
+            'locations'=>$loctions
+        ));
+
+
+
+
+    }
+
 }
