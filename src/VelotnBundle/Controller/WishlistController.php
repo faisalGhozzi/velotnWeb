e5ed4dd2b9ed7508f16e1bd112273abac6b76a70
faisalGhozzi <faicel.ghozzi@esprit.tn>
2020-03-30 21:39:58 +0100

    cart updates

--- src/VelotnBundle/Controller/WishlistController.php
+++ src/VelotnBundle/Controller/WishlistController.php
@@ -3,19 +3,43 @@
 namespace VelotnBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use VelotnBundle\Entity\Produits;
+use VelotnBundle\Entity\User;
 use VelotnBundle\Entity\Wishlist;
 
 class WishlistController extends Controller{
     /**
-     * @Route("/shop/wishlist/add",name="whishlistadd")
-     * @param $_route
+     * @Route("/AjouterWishlist",name="ajouterWishlist")
      * @param Request $request
+     * @return JsonResponse
      */
-/*public function wishlistaddAction($_route,Request $request){
+    public function AjouterWishlistAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $product = $em->getRepository(Produits::class)->find($request->request->get("idProduct"));
+        $u = $em->getRepository(User::class)->find($user);
 
-    $wishlist = new Wishlist();
+        $p= new Produits();
 
-}*/
+        $p->setId($product->getId());
+        $p->setNomprod($product->getNomprod());
+        $p->setDescription($product->getDescription());
+        $p->setPrix($product->getPrix());
+        $p->setQuantite($product->getQuantite());
+        $p->setImgUrl($product->getImgUrl());
+
+        $wishlist = new Wishlist();
+        $wishlist->setProduit($product);
+        $wishlist->setUser($u);
+
+        $em->persist($wishlist);
+        $em->flush();
+
+        return new JsonResponse();
+
+    }
 
 }
